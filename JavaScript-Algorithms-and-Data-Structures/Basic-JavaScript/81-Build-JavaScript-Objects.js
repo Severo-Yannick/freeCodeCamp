7ce21822a73f67dea2d5b24a2ade2f97b472ff26
Yannick Severo <severoyannick@gmail.com>
2021-07-29 22:11:57 +0200

    add solution

---
+++ JavaScript-Algorithms-and-Data-Structures/Basic-JavaScript/81-Build-JavaScript-Objects.js
@@ -0,0 +1,9 @@
+var myDog = {
+  // Only change code below this line
+    name: "toto",
+    legs: 4,
+    tails: 34,
+    friends: ["cats", "birds"]
+  // Only change code above this line
+  };
+  
