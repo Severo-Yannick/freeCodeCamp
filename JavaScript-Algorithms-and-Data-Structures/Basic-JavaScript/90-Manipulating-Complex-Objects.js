a5e0850c2b6cdef28b96f662c419b80a4b0456cf
Yannick Severo <severoyannick@gmail.com>
2021-08-01 14:57:29 +0200

    add solution

---
+++ JavaScript-Algorithms-and-Data-Structures/Basic-JavaScript/90-Manipulating-Complex-Objects.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Toto",
+    "title": "The Birds",
+    "release_year": 1673,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+  }
+];
